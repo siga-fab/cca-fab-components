0c5a143aac46d36d82677bdf599193055f6144e1
Luiz <37978726+luizmariz@users.noreply.github.com>
2020-09-11 16:31:29 -0300

    feat(common-button): adiciona as propriedades de estilo do componente
    Refs #2

--- projects/common/src/lib/button/button.component.ts
+++ projects/common/src/lib/button/button.component.ts
@@ -1,19 +1,57 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  ViewChild,
+  AfterViewInit,
+  ElementRef,
+  Optional,
+  Attribute,
+} from '@angular/core';
 
 @Component({
   selector: 'cca-common-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
 })
-export class ButtonComponent implements OnInit {
-  @Input() type: string;
-  @Output() buttonClick: EventEmitter<void> = new EventEmitter();
+export class ButtonComponent implements OnInit, AfterViewInit {
+  @ViewChild('iconRef') icon!: ElementRef;
+  @ViewChild('textRef') text!: ElementRef;
 
-  constructor() {}
+  @Input() type?: 'outline' | 'fill';
+  @Input() disabled?: boolean;
+  @Output() buttonClick?: EventEmitter<void> = new EventEmitter();
+
+  protected animate = false;
+
+  protected hasIcon = false;
+  protected hasText = false;
+
+  constructor(
+    @Optional() @Attribute('secondary') public secondary,
+    @Optional() @Attribute('flat') public flat,
+    @Optional() @Attribute('warning') public warning,
+    @Optional() @Attribute('negative') public negative
+  ) {}
 
   ngOnInit() {}
 
+  ngAfterViewInit() {
+    this.hasIcon = this.icon.nativeElement.children.length > 0;
+    this.hasText = this.text.nativeElement.textContent !== '';
+  }
+
   onClick() {
     this.buttonClick.emit();
+
+    if (!this.animate) {
+      this.animate = true;
+
+      setTimeout(() => {
+        this.animate = false;
+      }, 850);
+    }
   }
 }
--- src/stories/common/Button.stories.ts
+++ src/stories/common/Button.stories.ts
@@ -1,4 +1,3 @@
-import { Component } from '@angular/core';
 import { moduleMetadata } from '@storybook/angular';
 import { CommonModule } from '@angular/common';
 import { action } from '@storybook/addon-actions';
@@ -15,12 +14,101 @@ export default {
   decorators: [
     moduleMetadata({
       imports: [CommonModule, ButtonModule, IconModule],
-      declarations: [],
     }),
   ],
 };
 
-export const Primary = () => ({
+export const Styles = () => ({
+  props: {
+    click: action('This was clicked OMG'),
+  },
+  template: `
+    <div style="display: flex; flex-flow: row wrap">
+
+      <!--Primary-->
+      <section style="padding: 1rem;">
+        <cca-common-button (buttonClick)="click($event)">
+          <cca-common-icon slot="icon">add</cca-common-icon>
+          <ng-container slot="text">primary</ng-container>
+        </cca-common-button>
+        <br>
+        <cca-common-button type="fill" (buttonClick)="click($event)">
+          <ng-container slot="text">primary</ng-container>
+        </cca-common-button>
+        <br>
+        <cca-common-button type="transparent" (buttonClick)="click($event)">
+          <cca-common-icon slot="icon">add</cca-common-icon>
+        </cca-common-button>
+      </section>
+
+      <!--Secondary-->
+      <section style="padding: 1rem;">
+        <cca-common-button secondary (buttonClick)="click($event)">
+          <cca-common-icon slot="icon">add</cca-common-icon>
+          <ng-container slot="text">secondary</ng-container>
+        </cca-common-button>
+        <br>
+        <cca-common-button secondary type="fill" (buttonClick)="click($event)">
+          <ng-container slot="text">secondary</ng-container>
+        </cca-common-button>
+        <br>
+        <cca-common-button secondary type="transparent" (buttonClick)="click($event)">
+          <cca-common-icon slot="icon">add</cca-common-icon>
+        </cca-common-button>
+      </section>
+
+      <!--Flat-->
+      <section style="padding: 1rem;">
+        <cca-common-button flat (buttonClick)="click($event)">
+          <cca-common-icon slot="icon">add</cca-common-icon>
+          <ng-container slot="text">flat</ng-container>
+        </cca-common-button>
+        <br>
+        <cca-common-button flat type="fill" (buttonClick)="click($event)">
+          <ng-container slot="text">flat</ng-container>
+        </cca-common-button>
+        <br>
+        <cca-common-button flat type="transparent" (buttonClick)="click($event)">
+          <cca-common-icon slot="icon">add</cca-common-icon>
+        </cca-common-button>
+      </section>
+
+      <!--Warning-->
+      <section style="padding: 1rem;">
+        <cca-common-button warning (buttonClick)="click($event)">
+          <cca-common-icon slot="icon">add</cca-common-icon>
+          <ng-container slot="text">warning</ng-container>
+        </cca-common-button>
+        <br>
+        <cca-common-button warning type="fill" (buttonClick)="click($event)">
+          <ng-container slot="text">warning</ng-container>
+        </cca-common-button>
+        <br>
+        <cca-common-button warning type="transparent" (buttonClick)="click($event)">
+          <cca-common-icon slot="icon">add</cca-common-icon>
+        </cca-common-button>
+      </section>
+
+      <!--Negative-->
+      <section style="padding: 1rem; background-color: #234B90; border-radius: .5rem">
+        <cca-common-button negative (buttonClick)="click($event)">
+          <cca-common-icon slot="icon">add</cca-common-icon>
+          <ng-container slot="text">negative</ng-container>
+        </cca-common-button>
+        <br>
+        <cca-common-button negative type="fill" (buttonClick)="click($event)">
+          <ng-container slot="text">negative</ng-container>
+        </cca-common-button>
+        <br>
+        <cca-common-button negative type="transparent" (buttonClick)="click($event)">
+          <cca-common-icon slot="icon">add</cca-common-icon>
+        </cca-common-button>
+      </section>
+    </div>
+`,
+});
+
+export const Outline = () => ({
   props: {
     click: action('This was clicked OMG'),
   },
@@ -29,5 +117,95 @@ export const Primary = () => ({
       <cca-common-icon slot="icon">add</cca-common-icon>
       <ng-container slot="text">nova</ng-container>
     </cca-common-button>
+    <br>
+    <cca-common-button (buttonClick)="click($event)">
+      <ng-container slot="text">nova</ng-container>
+    </cca-common-button>
+    <br>
+    <cca-common-button (buttonClick)="click($event)">
+      <cca-common-icon slot="icon">add</cca-common-icon>
+    </cca-common-button>
+  `,
+});
+
+export const Fill = () => ({
+  props: {
+    click: action('This was clicked OMG'),
+  },
+  template: `
+    <cca-common-button type="fill" (buttonClick)="click($event)">
+      <cca-common-icon slot="icon">add</cca-common-icon>
+      <ng-container slot="text">nova</ng-container>
+    </cca-common-button>
+    <br>
+    <cca-common-button type="fill" (buttonClick)="click($event)">
+      <ng-container slot="text">nova</ng-container>
+    </cca-common-button>
+    <br>
+    <cca-common-button type ="fill" (buttonClick)="click($event)">
+      <cca-common-icon slot="icon">add</cca-common-icon>
+    </cca-common-button>
+  `,
+});
+
+export const Transparent = () => ({
+  props: {
+    click: action('This was clicked OMG'),
+  },
+  template: `
+    <cca-common-button type="transparent" (buttonClick)="click($event)">
+      <cca-common-icon slot="icon">add</cca-common-icon>
+      <ng-container slot="text">nova</ng-container>
+    </cca-common-button>
+    <br>
+    <cca-common-button type="transparent" (buttonClick)="click($event)">
+      <ng-container slot="text">nova</ng-container>
+    </cca-common-button>
+    <br>
+    <cca-common-button type="transparent" (buttonClick)="click($event)">
+      <cca-common-icon slot="icon">add</cca-common-icon>
+    </cca-common-button>
+  `,
+});
+
+export const Warning = () => ({
+  props: {
+    click: action('This was clicked OMG'),
+  },
+  template: `
+    <cca-common-button warning (buttonClick)="click($event)">
+      <cca-common-icon slot="icon">add</cca-common-icon>
+      <ng-container slot="text">warning</ng-container>
+    </cca-common-button>
+    <br>
+    <cca-common-button warning type="fill" (buttonClick)="click($event)">
+      <ng-container slot="text">warning</ng-container>
+    </cca-common-button>
+    <br>
+    <cca-common-button warning type="transparent" (buttonClick)="click($event)">
+      <cca-common-icon slot="icon">add</cca-common-icon>
+    </cca-common-button>
+  `,
+});
+
+export const Negative = () => ({
+  props: {
+    click: action('This was clicked OMG'),
+  },
+  template: `
+    <section style="background-color: #234B90; padding: 1rem; height: 100vh">
+      <cca-common-button negative (buttonClick)="click($event)">
+        <cca-common-icon slot="icon">add</cca-common-icon>
+        <ng-container slot="text">negative</ng-container>
+      </cca-common-button>
+      <br>
+      <cca-common-button negative type="fill" (buttonClick)="click($event)">
+        <ng-container slot="text">negative</ng-container>
+      </cca-common-button>
+      <br>
+      <cca-common-button negative type="transparent" (buttonClick)="click($event)">
+        <cca-common-icon slot="icon">add</cca-common-icon>
+      </cca-common-button>
+    </section>
   `,
 });
