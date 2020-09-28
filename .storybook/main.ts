fdb359ad455f7e21669a576d1e8039fe442fd455
Raphael Santana <raphaelstn@gmail.com>
2020-09-28 20:59:55 -0300

    feat: add dependency for docs

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,11 @@
 module.exports = {
-  stories: ['../src/**/*.stories.@(ts|mdx)'],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(ts|mdx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-notes',
     '@storybook/addon-storysource',
     '@storybook/addon-actions',
     '@storybook/addon-backgrounds',
+    '@storybook/addon-docs',
   ],
 };
