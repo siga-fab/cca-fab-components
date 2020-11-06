5faa2c5ebccea930284f48cbe1cac0b158fd9a2c
Luiz <37978726+luizmariz@users.noreply.github.com>
2020-11-06 15:26:24 -0300

    feat(common-card): adiciona header, footer, e a prop de title no card

--- projects/common/src/lib/card/card.component.ts
+++ projects/common/src/lib/card/card.component.ts
@@ -1,12 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewChild,
+  AfterViewInit,
+  ChangeDetectorRef,
+  AfterViewChecked,
+} from '@angular/core';
 
 @Component({
   selector: 'com-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
-export class CardComponent implements OnInit {
-  constructor() {}
+export class CardComponent implements OnInit, AfterViewInit, AfterViewChecked {
+  /**
+   * @internal
+   */
+  @ViewChild('headerRef') header!: ElementRef;
+  /**
+   * @internal
+   */
+  @ViewChild('footerRef') footer!: ElementRef;
+
+  /**
+   * @internal
+   */
+  public hasHeader: boolean;
+  /**
+   * @internal
+   */
+  public hasFooter: boolean;
+
+  @Input() headerTitle?: string;
+
+  constructor(private cdref: ChangeDetectorRef) {}
 
   ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    this.hasFooter = this.footer.nativeElement.children.length > 0;
+    this.hasHeader =
+      this.header.nativeElement.children.length > 0 || !!this.headerTitle;
+  }
+
+  ngAfterViewChecked() {
+    this.cdref.detectChanges();
+  }
 }
--- src/stories/common/Card.stories.ts
+++ src/stories/common/Card.stories.ts
@@ -18,9 +18,21 @@ export default {
 
 export const Default = () => ({
   template: `
-    <div style="width: 30rem; height: 40rem;">
-      <com-card>
+    <div style="width: 30rem; margin: 1rem;">
+      <com-card >
         <span>This is a card!</span>
+      </com-card>
+    </div>
+    <div style="width: 30rem; margin: 1rem;">
+      <com-card headerTitle="This is a card title">
+        <span>This is a card!</span>
+      </com-card>
+    </div>
+    <div style="width: 30rem; margin: 1rem;">
+      <com-card>
+        <div slot="header">This is a header</div>
+        <span>This is a card!</span>
+        <div slot="footer">This is a footer</div>
       </com-card>
     </div>
   `,
