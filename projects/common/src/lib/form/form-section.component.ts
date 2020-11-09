1fc7eb4430cec682a5642550e45f58a269bb602e
Matheus <matheus.correia.sg@gmail.com>
2020-11-09 17:25:18 -0300

    feat(common-form-section): adiciona reatividade aos atributos opcionais HTML

--- projects/common/src/lib/form/form-section.component.ts
+++ projects/common/src/lib/form/form-section.component.ts
@@ -1,3 +1,4 @@
+import { ChangeDetectorRef, OnDestroy } from '@angular/core';
 import {
   Component,
   OnInit,
@@ -5,6 +6,7 @@ import {
   Attribute,
   HostBinding,
   Input,
+  ElementRef,
 } from '@angular/core';
 
 @Component({
@@ -12,7 +14,7 @@ import {
   templateUrl: './form-section.component.html',
   styleUrls: ['./form-section.component.scss'],
 })
-export class FormSectionComponent implements OnInit {
+export class FormSectionComponent implements OnInit, OnDestroy {
   @HostBinding('class.partly-sized') partlySized = false;
 
   @Input() description?: string;
@@ -22,12 +24,47 @@ export class FormSectionComponent implements OnInit {
   // A number between 0-99
   @Input() step?: number;
 
+  _observer: MutationObserver;
+
   constructor(
     @Optional() @Attribute('partly') public partly,
-    @Optional() @Attribute('block') public block
-  ) {}
+    @Optional() @Attribute('block') public block,
+    private elRef: ElementRef
+  ) {
+    this.partly = partly !== null;
+    this.block = block !== null;
+
+    this._observer = new MutationObserver((mutations: MutationRecord[]) => {
+      for (const mutation of mutations) {
+        if (mutation.attributeName === 'partly') {
+          this.partly =
+            this.elRef.nativeElement.attributes.getNamedItem('partly') !== null;
+          this.partlySized = this.partly;
+          continue;
+        }
+
+        if (mutation.attributeName === 'block') {
+          console.log(
+            this.elRef.nativeElement.attributes.getNamedItem('block')
+          );
+          this.block =
+            this.elRef.nativeElement.attributes.getNamedItem('block') !== null;
+          continue;
+        }
+      }
+    });
+
+    this._observer.observe(elRef.nativeElement, {
+      attributes: true,
+      attributeOldValue: true,
+    });
+  }
 
   ngOnInit(): void {
-    this.partlySized = this.partly === '';
+    this.partlySized = this.partly;
+  }
+
+  ngOnDestroy(): void {
+    this._observer.disconnect();
   }
 }
